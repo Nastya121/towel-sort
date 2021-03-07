449389debedb950e1c3035fb0636ec35281ed84e
Nastya121 <77975134+Nastya121@users.noreply.github.com>
2021-03-07 21:23:30 +0400

    feat : add solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-
-// You should implement your task here.
-
-
-
 module.exports = function towelSort (matrix) {
 let newArr = [];
-
-
+     if (matrix==null) {return [];} 
 for(let i=0; i<matrix.lenght; i++){
 if(i%2==0){
 for(let j=0; j<matrix[i].lenght; j++){ 
     newArr.push(matrix[i][j]);
 }
 }else{
-
   for(let j=matrix[i].lenght-1; 0 <=j; j--){ 
   newArr.push(matrix[i][j]);
      }
@@ -21,6 +14,21 @@ for(let j=0; j<matrix[i].lenght; j++){
 }
 return newArr;
 }
+/*------------
+module.exports = function towelSort (matrix) {
+  let newArr = [];
+  if (matrix==null) {return [];} 
+  for (let i = 0; i < matrix.length; i++) {
+     if (i%2 == 0) {      
+      for (let j = 0; j < matrix[i].length; j++) {
+        newArr.push(matrix[i][j]); 
+        }}
+    else {
+      for (let j =  matrix[i].length-1; 0 <=j ; j--) {
+        newArr.push(matrix[i][j]);
+   } }}
+  return newArr;
+}*/
 
 
 

b025916aa56a9a49a47d90a478cfb78bad6569da
Nastya121 <77975134+Nastya121@users.noreply.github.com>
2021-03-07 21:34:41 +0400

    feat : add solution

--- src/index.js
+++ src/index.js
@@ -14,21 +14,6 @@ for(let j=0; j<matrix[i].length; j++){
 }
 return newArr;
 }
-/*------------
-module.exports = function towelSort (matrix) {
-  let newArr = [];
-  if (matrix==null) {return [];} 
-  for (let i = 0; i < matrix.length; i++) {
-     if (i%2 == 0) {      
-      for (let j = 0; j < matrix[i].length; j++) {
-        newArr.push(matrix[i][j]); 
-        }}
-    else {
-      for (let j =  matrix[i].length-1; 0 <=j ; j--) {
-        newArr.push(matrix[i][j]);
-   } }}
-  return newArr;
-}*/
 
 
 
